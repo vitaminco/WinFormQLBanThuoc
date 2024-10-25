6a51904fb54e486582c9fd02d0ad80bf50878a39
Hung Huynh <khanhhung89890@gmail.com>
2024-10-26 00:43:50 +0700

    add be employ

--- AppQLNhaThuoc/Employ.Designer.cs
+++ AppQLNhaThuoc/Employ.Designer.cs
@@ -29,19 +29,19 @@
         private void InitializeComponent()
         {
             this.Option = new System.Windows.Forms.GroupBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.delEmploy = new System.Windows.Forms.Button();
-            this.updateEmploy = new System.Windows.Forms.Button();
-            this.addEmploy = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.delEmploy = new System.Windows.Forms.Button();
+            this.updateEmploy = new System.Windows.Forms.Button();
+            this.addEmploy = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtcode = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Option.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -57,96 +57,15 @@
             this.Option.Controls.Add(this.updateEmploy);
             this.Option.Controls.Add(this.addEmploy);
             this.Option.Controls.Add(this.textBox4);
-            this.Option.Controls.Add(this.textBox3);
-            this.Option.Controls.Add(this.textBox2);
-            this.Option.Controls.Add(this.textBox1);
+            this.Option.Controls.Add(this.txtphone);
+            this.Option.Controls.Add(this.txtname);
+            this.Option.Controls.Add(this.txtcode);
             this.Option.Location = new System.Drawing.Point(9, 4);
             this.Option.Name = "Option";
             this.Option.Size = new System.Drawing.Size(785, 169);
             this.Option.TabIndex = 3;
             this.Option.TabStop = false;
             this.Option.Text = "Option";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(175, 127);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(376, 22);
-            this.textBox4.TabIndex = 3;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(175, 91);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(375, 22);
-            this.textBox3.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(175, 56);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(376, 22);
-            this.textBox2.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(175, 21);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(377, 22);
-            this.textBox1.TabIndex = 0;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.dataGridView1);
-            this.groupBox1.Location = new System.Drawing.Point(5, 173);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(791, 273);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Data";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 18);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(790, 269);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // delEmploy
-            // 
-            this.delEmploy.BackColor = System.Drawing.Color.OliveDrab;
-            this.delEmploy.ForeColor = System.Drawing.Color.White;
-            this.delEmploy.Location = new System.Drawing.Point(638, 107);
-            this.delEmploy.Name = "delEmploy";
-            this.delEmploy.Size = new System.Drawing.Size(122, 30);
-            this.delEmploy.TabIndex = 8;
-            this.delEmploy.Text = "Xóa";
-            this.delEmploy.UseVisualStyleBackColor = false;
-            // 
-            // updateEmploy
-            // 
-            this.updateEmploy.BackColor = System.Drawing.Color.OliveDrab;
-            this.updateEmploy.ForeColor = System.Drawing.Color.White;
-            this.updateEmploy.Location = new System.Drawing.Point(638, 71);
-            this.updateEmploy.Name = "updateEmploy";
-            this.updateEmploy.Size = new System.Drawing.Size(122, 30);
-            this.updateEmploy.TabIndex = 9;
-            this.updateEmploy.Text = "Sửa";
-            this.updateEmploy.UseVisualStyleBackColor = false;
-            // 
-            // addEmploy
-            // 
-            this.addEmploy.BackColor = System.Drawing.Color.OliveDrab;
-            this.addEmploy.ForeColor = System.Drawing.Color.White;
-            this.addEmploy.Location = new System.Drawing.Point(638, 35);
-            this.addEmploy.Name = "addEmploy";
-            this.addEmploy.Size = new System.Drawing.Size(122, 30);
-            this.addEmploy.TabIndex = 10;
-            this.addEmploy.Text = "Thêm";
-            this.addEmploy.UseVisualStyleBackColor = false;
             // 
             // label4
             // 
@@ -184,6 +103,87 @@
             this.label1.TabIndex = 15;
             this.label1.Text = "Tên chi nhánh";
             // 
+            // delEmploy
+            // 
+            this.delEmploy.BackColor = System.Drawing.Color.OliveDrab;
+            this.delEmploy.ForeColor = System.Drawing.Color.White;
+            this.delEmploy.Location = new System.Drawing.Point(638, 107);
+            this.delEmploy.Name = "delEmploy";
+            this.delEmploy.Size = new System.Drawing.Size(122, 30);
+            this.delEmploy.TabIndex = 8;
+            this.delEmploy.Text = "Xóa";
+            this.delEmploy.UseVisualStyleBackColor = false;
+            // 
+            // updateEmploy
+            // 
+            this.updateEmploy.BackColor = System.Drawing.Color.OliveDrab;
+            this.updateEmploy.ForeColor = System.Drawing.Color.White;
+            this.updateEmploy.Location = new System.Drawing.Point(638, 71);
+            this.updateEmploy.Name = "updateEmploy";
+            this.updateEmploy.Size = new System.Drawing.Size(122, 30);
+            this.updateEmploy.TabIndex = 9;
+            this.updateEmploy.Text = "Sửa";
+            this.updateEmploy.UseVisualStyleBackColor = false;
+            // 
+            // addEmploy
+            // 
+            this.addEmploy.BackColor = System.Drawing.Color.OliveDrab;
+            this.addEmploy.ForeColor = System.Drawing.Color.White;
+            this.addEmploy.Location = new System.Drawing.Point(638, 35);
+            this.addEmploy.Name = "addEmploy";
+            this.addEmploy.Size = new System.Drawing.Size(122, 30);
+            this.addEmploy.TabIndex = 10;
+            this.addEmploy.Text = "Thêm";
+            this.addEmploy.UseVisualStyleBackColor = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(175, 127);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(376, 22);
+            this.textBox4.TabIndex = 3;
+            // 
+            // txtphone
+            // 
+            this.txtphone.Location = new System.Drawing.Point(175, 91);
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(375, 22);
+            this.txtphone.TabIndex = 2;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(175, 56);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(376, 22);
+            this.txtname.TabIndex = 1;
+            // 
+            // txtcode
+            // 
+            this.txtcode.Location = new System.Drawing.Point(175, 21);
+            this.txtcode.Name = "txtcode";
+            this.txtcode.Size = new System.Drawing.Size(377, 22);
+            this.txtcode.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 173);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(791, 273);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(790, 269);
+            this.dataGridView1.TabIndex = 0;
+            // 
             // Employ
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -205,9 +205,9 @@
 
         private System.Windows.Forms.GroupBox Option;
         private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtphone;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtcode;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button delEmploy;
--- AppQLNhaThuoc/Employ.cs
+++ AppQLNhaThuoc/Employ.cs
@@ -1,20 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AppQLNhaThuoc
 {
     public partial class Employ : Form
     {
+        function f = new function();
+        string query;
         public Employ()
         {
             InitializeComponent();
+        }
+
+        private void addEmploy_Click(object sender, EventArgs e)
+        {
+            if (txtcode.Text == "" || txtname.Text == "" || txtphone.Text == "")
+            {
+                MessageBox.Show("Hãy nhập đủ thông tin", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                string code = txtcode.Text;
+                string name = txtname.Text;
+                string phone = txtphone.Text;
+                query = "INSERT INTO Employee (EmployeeCode, FullName, Phone) VALUES ('" + code + "', N'" + name + "', '" + phone + "')";
+                f.setData(query, "Thêm nhân viên thành công");
+                Employ_Load(this, null);
+            }
+        }
+
+        private void updateEmploy_Click(object sender, EventArgs e)
+        {
+            if (txtcode.Text == "" || txtname.Text == "" || txtphone.Text == "")
+            {
+                MessageBox.Show("Hãy nhập đủ thông tin", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                string code = txtcode.Text;
+                string name = txtname.Text;
+                string phone = txtphone.Text;
+                string query = "UPDATE Employee SET EmployeeCode = '" + code + "', FullName = N'" + name + "', Phone = '" + phone + "' WHERE EmployeeCode = '" + code + "'";
+                f.setData(query, "Cập nhật thông tin nhân viên thành công");
+                Employ_Load(this, null);
+            }
+        }
+
+        private void delEmploy_Click(object sender, EventArgs e)
+        {
+            string code = txtcode.Text;
+            string query = "DELETE FROM Employee WHERE EmployeeCode = '" + code + "'";
+
+            f.setData(query, "Xóa nhân viên thành công");
+            Employ_Load(this, null);
+        }
+
+        private void Employ_Load(object sender, EventArgs e)
+        {
+            query = "select * from Employee";
+            DataSet ds = f.getData(query);
+            dataGridView1.DataSource = ds.Tables[0];
         }
     }
 }
